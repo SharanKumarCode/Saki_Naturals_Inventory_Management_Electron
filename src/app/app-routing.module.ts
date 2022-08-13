95b0f7cf6143b321f736881c6df3c6ac4768b5d7
SHARAN KUMAR <45118936+SharanKumarCode@users.noreply.github.com>
2022-08-13 23:37:19 +0530

    implemented routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductsComponent } from './products/products.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { SalesComponent } from './sales/sales.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { SettingsComponent } from './settings/settings.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'expense', component: ExpenseComponent },
+  { path: 'sales', component: SalesComponent },
+  { path: 'purchase', component: PurchaseComponent },
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'settings', component: SettingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
